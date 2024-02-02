bc71c1bcf22bf061f48282466b82a09a31b10ca1
Fayiz Asad <fayiz.asad@infosoft.ae>
2024-02-02 16:32:41 +0400

    empty

--- sally-flower-shop/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import flower_shop
-from . import product_template
-from . import flower_shop_water
-from . import stock_lot
-from . import stock_warehouse_weather
